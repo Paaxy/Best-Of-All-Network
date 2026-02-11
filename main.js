415bfeae73c05a9974a3d04452e6d9db2da4e0a4
Paaxy <paaxpaul@gmail.com>
2026-02-11 15:31:49 +0900

    ssd

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/sw.js')
       .then(reg => console.log('Service worker registered!', reg))
       .catch(err => console.error('Service worker registration failed:', err));
   });
