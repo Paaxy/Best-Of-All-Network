0213248b451f3fd262b1008980e33ca94a1c3dfe
Paaxy <163489393+Paaxy@users.noreply.github.com>
2026-03-21 14:20:28 +0900

    dfdfdfdfdfd

--- main.js
+++ main.js
@@ -50,3 +50,14 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
+} else {
+  console.error("Service workers are not supported in this browser.");
+}
